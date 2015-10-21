286b85b1213940530f6b5d4e7e374e1beb20cfa0
Axelelele <axel.jonsson94@live.se>
2015-10-21 11:03:24 +0200

    Added questionservlet
    i'm dying inside

---
+++ src/projectmissinformation/controller/QuestionServlet.java
@@ -0,0 +1,49 @@
+package projectmissinformation.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import projectmissinformation.model.DBHandler;
+import projectmissinformation.model.Question;
+import projectmissinformation.model.User;
+
+/**
+ * @author Axel
+ * Servlet implementation class QuestionServlet
+ */
+@WebServlet("/QuestionServlet")
+public class QuestionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private String name;
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		DBHandler dbHandler = new DBHandler();
+		
+		Cookie[] Cookies = request.getCookies();
+		if (Cookies != null) {
+			for (Cookie c : Cookies) {
+				if (c.getName().equals("user")) {
+					User u = dbHandler.getUser(c.getName());
+					List<Question> questionList = dbHandler.listQuestions(u);
+					request.setAttribute("questionList", questionList);
+					request.setAttribute("adminStatus", u.getAdmin());
+					RequestDispatcher rd = request.getRequestDispatcher("/ProjectMissInformation/login.jsp");
+					rd.forward(request, response);
+				}
+			}
+		}
+	}
+
+}
